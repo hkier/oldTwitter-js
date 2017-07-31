b38a57b0f048c1f5069302fae9fca2ed9ddfde4c
Howard Kier <howard@Howards-MacBook-Air.local>
2017-07-31 17:30:19 -0500

    Volleyball integrated

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 
+const volleyball = require('volleyball');
+const morgan = require ('morgan');
+
+app.use(volleyball);
+//app.use(morgan);
 
 app.get('/', function (req,res) {
   res.send('Dave.... What can I do for you?')
 }
 )
+
+app.get('/news', function (req,res) {
+  res.send('All the news that fit to print')
+}
+)
+
 
 
 app.listen(3000, function () {
