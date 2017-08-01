4edb4657435a648cb8e81a65fba53b8b06ffdd96
Robert Shaw <r.shaw1@yahoo.com>
2017-08-01 09:53:28 -0500

    setup rendering

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const express = require( 'express' );
+const express = require('express');
 const app = express(); // creates an instance of an express application
 
 const nunjucks = require('nunjucks');
@@ -8,30 +8,31 @@ const volleyball = require('volleyball');
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
 
 
 let locals = {
   title: 'An Example',
   people: [
-      { name: 'Gandalf'},
-      { name: 'Frodo' },
-      { name: 'Hermione'}
+    { name: 'Gandalf' },
+    { name: 'Frodo' },
+    { name: 'Hermione' }
   ]
 };
 
 nunjucks.render('index.html', locals, function (err, output) {
-    console.log(output);
+  console.log(output);
 });
 
 
 app.use(volleyball);
 
-app.get('/', function (req,res) {
-  res.send('Dave.... What can I do for you?')
+app.get('/', function (req, res) {
+  const people = [{ name: 'Full' }, { name: 'Stacker' }, { name: 'Son' }];
+  res.render('index', { title: 'Hall of Fame', people: people });
 })
 
-app.get('/news', function (req,res) {
+app.get('/news', function (req, res) {
   res.send('All the news that fit to print')
 })
 
