142af7be69a98925abe8e77b3cb868cc1ca0fb9a
Robert Shaw <r.shaw1@yahoo.com>
2017-08-01 10:30:14 -0500

    added basic functionality

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const express = require('express');
 const app = express(); // creates an instance of an express application
 
 const nunjucks = require('nunjucks');
-
 const volleyball = require('volleyball');
+
+const tweetBank = require('./tweetBank')
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
 
+
+// let data = tweetBank.data;
+// tweetBank.add('Robert', 'Have a great day!')
+// console.log(tweetBank.list())
+
+// // tweetBank.find('Robert')
+// let properties = function(o) {
+//   return o.name === 'Robert';
+// }
+// console.log('find method = ', tweetBank.find(properties))
 
 let locals = {
   title: 'An Example',
