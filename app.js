f49a9225cc88be8fa86a797aa5c02bf28b55079e
Robert Shaw <r.shaw1@yahoo.com>
2017-08-01 15:21:25 -0500

    refactored server for use with socket io

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-
+//node modules
 const express = require('express');
 const app = express(); // creates an instance of an express application
 const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
 const volleyball = require('volleyball');
+const socketio = require('socket.io');
 
-
+//local modules
 const tweetBank = require('./tweetBank')
 const routes = require('./routes');
+
+const server = app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+  console.log(Date());
+});
+
+const io = socketio.listen(server);
 
 //middleware
 app.use(volleyball);
@@ -17,15 +25,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 // parse application/json
 app.use(bodyParser.json())
 
-app.use('/', routes);
+app.use('/', routes(io));
 app.use(express.static('public'))
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-  console.log(Date());
-})
-
