db0104aed21c484b06b2a1ae4ef3da02c9a903ef
Robert Shaw <r.shaw1@yahoo.com>
2017-08-01 14:54:55 -0500

    added body-parser middleware

--- app.js
+++ app.js
@@ -1,49 +1,28 @@
 
 const express = require('express');
 const app = express(); // creates an instance of an express application
-
+const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
 const volleyball = require('volleyball');
 
-const tweetBank = require('./tweetBank')
 
+const tweetBank = require('./tweetBank')
 const routes = require('./routes');
+
+//middleware
+app.use(volleyball);
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false})); 
+// parse application/json
+app.use(bodyParser.json())
+
 app.use('/', routes);
+app.use(express.static('public'))
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
-
-app.use(express.static('public'))
-
-// let data = tweetBank.data;
-// tweetBank.add('Robert', 'Have a great day!')
-// console.log(tweetBank.list())
-
-// // tweetBank.find('Robert')
-// let properties = function(o) {
-//   return o.name === 'Robert';
-// }
-// console.log('find method = ', tweetBank.find(properties))
-
-let locals = {
-  title: 'An Example',
-  people: [
-    { name: 'Gandalf' },
-    { name: 'Frodo' },
-    { name: 'Hermione' }
-  ]
-};
-
-nunjucks.render('index.html', locals, function (err, output) {
- // console.log(output);
-});
-
-
-app.use(volleyball);
-
-
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
