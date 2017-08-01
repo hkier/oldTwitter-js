8c949cf6a282da52f24e3bf3fb8a1e25fc77961f
Howard Kier <howard@Howards-MacBook-Air.local>
2017-08-01 11:31:59 -0500

    Test with Route is working

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const volleyball = require('volleyball');
 
 const tweetBank = require('./tweetBank')
 
+const routes = require('./routes');
+app.use('/', routes);
+
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
 
+app.use(express.static('public'))
 
 // let data = tweetBank.data;
 // tweetBank.add('Robert', 'Have a great day!')
@@ -32,23 +36,17 @@ let locals = {
 };
 
 nunjucks.render('index.html', locals, function (err, output) {
-  console.log(output);
+ // console.log(output);
 });
 
 
 app.use(volleyball);
 
-app.get('/', function (req, res) {
-  const people = [{ name: 'Full' }, { name: 'Stacker' }, { name: 'Son' }];
-  res.render('index', { title: 'Hall of Fame', people: people });
-})
 
-app.get('/news', function (req, res) {
-  res.send('All the news that fit to print')
-})
 
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
+  console.log(Date());
 })
 
